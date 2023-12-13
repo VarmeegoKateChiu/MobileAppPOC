6e3ea63735b6fdec45138c71caf30f0c4d9e529c
rufus <ufb04307@gmail.com>
2023-12-13 15:32:58 +0800

    update: firebase setting

--- src/Screens/BottomTabPages/NorificationPage.tsx
+++ src/Screens/BottomTabPages/NorificationPage.tsx
@@ -1,9 +1,40 @@
 import {Text, View} from "react-native";
-import React from "react";
+import React, {useEffect} from "react";
+import messaging from '@react-native-firebase/messaging';
 
 function NotificationPage() {
-    return (
+    useEffect(() => {
+        // iOS permission
+        async function requestPermission() {
+            const authStatus = await messaging().requestPermission();
+            const enabled =
+              authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+              authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+            if (enabled) {
+                getFcmToken();
+            }
+        }
+        requestPermission();
+    }, []);
 
+    useEffect(() => {
+        const unsubscribe = messaging().onMessage(async remoteMessage => {
+            const notification = remoteMessage.notification;
+            console.log('message: ', notification);
+        });
+        return unsubscribe;
+    }, []);
+
+    async function getFcmToken() {
+        try {
+            const fcmToken = await messaging().getToken();
+            console.log('fcmToken: ', fcmToken);
+        } catch (error) {
+            console.log('error in fcmtoken', error);
+        }
+    }
+
+    return (
 
         <Text>NotificationPage page</Text>
 
