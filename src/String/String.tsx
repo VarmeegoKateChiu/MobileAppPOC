dd3decd7960e527ddd8698bff43c5adbaa9cdcfe
Kate Chiu <43260717+VarmeegoKateChiu@users.noreply.github.com>
2023-12-13 11:59:29 +0800

    [Structure]Update: Add localized String

---
+++ src/String/String.tsx
@@ -0,0 +1,16 @@
+import LocalizedStrings from 'react-native-localization';
+
+export default new LocalizedStrings({
+    'en'   :{
+        login : 'Login',
+        logout: 'Logout',
+
+        landingTitle:'Landing Page (May Be Promotion Page)'
+    },
+    'zh_TW':{
+        login : '登入', 
+        logout: '登出',
+
+        landingTitle:'Landing Page (May Be Promotion Page)'
+    }
+});
